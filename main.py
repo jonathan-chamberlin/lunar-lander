3153c883063297fa82cf7dc60ccf69e18f20cb48
Jonathan Chamberlin <jcham17x@gmail.com>
2025-12-19 02:19:37 -0500

    added class for OUActionNoise

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hey')
+import os 
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+class OUActionNoise(object): def __init__(self, mu, sigma=0.15, theta=0.2,dt=1e-2,x0=None):
+    self.theta = theta
+    self.mu = mu
+    self.sigma = sigma
+    self.dt = dt
+    self.x0 = x0
+    self.reset()
