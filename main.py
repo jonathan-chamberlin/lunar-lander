f1bed39f970e9d3c0561318543fda8bb8dce9898
Jonathan Chamberlin <jcham17x@gmail.com>
2025-12-20 16:45:56 -0500

    formatting

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ class OUActionNoise(object):
         self.sigma = sigma
         self.dt = dt
         self.x0 = x0
+        self.reset()
+
+# create method within OUAActionNoise
 
 class Customer(object):
     def __init__(self, name, email, phone):
         self.name = name
         self.email = email
         self.phone = phone
+        self.reset()
     def send_sms(self):
         print(f"Send a sms to {self.name}'s number of {self.phone}")
 
