78572aaa91e764738ee6b2b02c91ae5347b84824
Jonathan Chamberlin <jcham17x@gmail.com>
2025-12-20 16:26:13 -0500

    learned how to define functions within a class

--- main.py
+++ main.py
@@ -5,10 +5,21 @@ import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 
-class OUActionNoise(object): def __init__(self, mu, sigma=0.15, theta=0.2,dt=1e-2,x0=None):
-    self.theta = theta
-    self.mu = mu
-    self.sigma = sigma
-    self.dt = dt
-    self.x0 = x0
-    self.reset()
+class OUActionNoise(object):
+    def __init__(self, mu, sigma=0.15, theta=0.2,dt=1e-2,x0=None):
+        self.theta = theta
+        self.mu = mu
+        self.sigma = sigma
+        self.dt = dt
+        self.x0 = x0
+
+class Customer(object):
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+    def send_sms(self):
+        print(f"Send a sms to {self.name}'s number of {self.phone}")
+
+customer1 = Customer("Jonathan", "jcham17x@gmail.com", 5426783357)
+customer1.send_sms()
