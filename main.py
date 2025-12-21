7e074b257d8f9d18f3354fc872159d53e19ed704
Jonathan Chamberlin <jcham17x@gmail.com>
2025-12-21 16:04:07 -0500

    installed python 3.12.5, because i can only use a box2d package with that version of python. figured out how to create and select another virtual environment too

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+
+
+
+import warnings
 import os 
 import torch as T
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
+
+warnings.filterwarnings("ignore")
 
 class OUActionNoise(object):
     def __init__(self, mu, sigma=0.15, theta=0.2,dt=1e-2,x0=None):
@@ -12,7 +18,6 @@ class OUActionNoise(object):
         self.sigma = sigma
         self.dt = dt
         self.x0 = x0
-        self.reset()
 
 # create method within OUAActionNoise
 
@@ -21,9 +26,16 @@ class Customer(object):
         self.name = name
         self.email = email
         self.phone = phone
-        self.reset()
+        
     def send_sms(self):
         print(f"Send a sms to {self.name}'s number of {self.phone}")
 
 customer1 = Customer("Jonathan", "jcham17x@gmail.com", 5426783357)
 customer1.send_sms()
+
+import gymnasium as gym
+
+# Try this and observe what happens
+env = gym.make("LunarLander-v3", render_mode="human")
+
+env.reset()
