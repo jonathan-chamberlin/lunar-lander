9c6e85ee9d2b27a5d91c871598854160e0c4b3af
Jonathan Chamberlin <jcham17x@gmail.com>
2025-12-21 22:01:07 -0500

    now only one environemnt is initialized and it doens't initialize a new environment every frame

--- main.py
+++ main.py
@@ -38,22 +38,18 @@ import gymnasium as gym
 pg.init()
 # Try this and observe what happens
 
-
+env = gym.make("LunarLander-v3", render_mode="human")
+env.reset()
 running = True
-while running:
-    
-    env = gym.make("LunarLander-v3", render_mode="human")
-    
+while running:    
     events = pg.event.get()
     for event in events:
         if event.type == 256:
             running = False
             env.close()
+    # print(f"Event type: {event.type}")
+    # print(f"Event object: {event}")
     
-    print(f"Event type: {event.type}")
-    print(f"Event object: {event}")
-    env.reset()
-    env.render()
     
     
     
